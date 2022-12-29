7f847c79945413012295a11154c11e8da096ccd1
sudonorm <sudonorm@protonmail.com>
2022-12-30 00:45:06 +0100

    add test files

---
+++ fol/testone.py
@@ -0,0 +1,9 @@
+def function(name, default=None, *args, variable="1123", a, b, c, employee, office, d, e, f, **kwargs):
+    """This is function is created to demonstrate using an action to format python files using black"""
+ 
+ 
+string = 'Geeks'
+ 
+j = [1,
+ 2,
+ 3]
