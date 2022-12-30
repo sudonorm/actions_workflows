ddc9db6c9a03a85dba3b3e76a8fb9fb184105466
sudonorm <sudonorm@users.noreply.github.com>
2022-12-30 09:07:52 +0000

    Automated black fixes

--- fol/testone.py
+++ fol/testone.py
@@ -1,9 +1,21 @@
-def function(name, default=None, *args, variable="1123", a, b, c, employee, office, d, e, f, **kwargs):
+def function(
+    name,
+    default=None,
+    *args,
+    variable="1123",
+    a,
+    b,
+    c,
+    employee,
+    office,
+    d,
+    e,
+    f,
+    **kwargs
+):
     """This is function is created to demonstrate using an action to format python files using black"""
- 
- 
-string = 'Geeks'
- 
-j = [1,
- 2,
- 3]
+
+
+string = "Geeks"
+
+j = [1, 2, 3]
